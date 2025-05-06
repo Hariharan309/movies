d769e9a241f78d71bf20980b7e0cb180a0ba4522
Y********* <Hariharan309@gmail.com>
2025-05-06 11:16:29 +0530

    route structure

--- main.js
+++ main.js
@@ -4,6 +4,17 @@ const port=3000
 app.get('/',(req,res)=>{
     res.send('Hello World')
 })
+
+app.get('/movies',(req,res)=>{})
+
+app.post('/movies',(req,res)=>{})
+
+app.put('/movies',(req,res)=>{})
+
+app.delete('/movies',(req,res)=>{})
+
+
+
 app.listen(port,()=>console.log(`Example app listening on port ${port}!`))
 
 module.exports=app
